05dda518bb686431a9030bb14b9d9d32943e3da5
Carles <carmar85@gmail.com>
2024-11-21 18:35:19 +0100

    initial commit, controllers, config, types, task, project, models

---
+++ src/controllers/ProjectController.ts
@@ -0,0 +1,67 @@
+import type { Request, Response } from "express";
+import Project from "../models/Project";
+
+export class ProjectController {
+  static createProject = async (req: Request, res: Response) => {
+    const project = new Project(req.body);
+    try {
+      await project.save();
+      res.send("Project created");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  static getAllProjects = async (req: Request, res: Response) => {
+    try {
+      const projects = await Project.find({});
+      res.json(projects);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  static getProjectById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const project = await Project.findById(id).populate("tasks");
+      if (!project) {
+        const error = new Error("Project not found");
+        res.status(404).json({ error: error.message });
+      }
+      res.json(project);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  static updateProject = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const project = await Project.findByIdAndUpdate(id, req.body);
+      if (!project) {
+        const error = new Error("Project not found");
+        res.status(404).json({ error: error.message });
+      }
+      await project.save();
+      res.send(project);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  static deleteProject = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const project = await Project.findById(id);
+      if (!project) {
+        const error = new Error("Project not found");
+        res.status(404).json({ error: error.message });
+      }
+      await project.deleteOne();
+      res.send("Project deleted");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
